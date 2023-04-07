2a6a521c66ea5ec6fb6a42d03383e486d42bd0d1
MarviMemon <marvimemon283@gmail.com>
2023-04-07 18:09:31 +0500

    js assignment sir

---
+++ App.js
@@ -0,0 +1,113 @@
+//============Q1===========
+
+//let num_1 = +prompt("enter no 1:");
+//let num_2 = +prompt("enter no 2:");
+//if(num_1 > num_2){
+    //console.log(num_1 + " is greater");    
+//}
+//else if(num_2 > num_1){
+    //console.log(num_2 + " is greater");
+//}
+//else{
+    //console.log("both numbers are equal");
+//}
+
+
+//===========Q2===========
+//let num = +prompt("enter no:");
+//if (num > 0){
+    //console.log("+ sign");
+//}
+//else if (num < 0){
+    //console.log("- sign");
+//}
+//else if ( num === 0){
+    //console.log("entered no is zero");
+//}
+
+//=========Q3===========
+
+//let num_1 = +prompt("enter no 1:");
+//let num_2 = +prompt("enter no 2:");
+//let num_3 = +prompt("enter no 3:");
+//let num_4 = +prompt("enter no 4:");
+//let num_5 = +prompt("enter no 5:");
+
+//if (num_1 > num_2 && num_1 > num_3 && num_1 > num_4 && num_1 > num_5){
+    //console.log(num_1 + " is greatest")
+//}
+//else if (num_2 > num_1 && num_2 > num_3 && num_2 > num_4 && num_2 > num_5){
+   // console.log(num_2 + " is greatest")
+//}
+//else if (num_3 > num_1 && num_3 > num_2 && num_3 > num_4 && num_3 > num_5){
+    //console.log(num_3 + " is greatest")
+//}
+//else if (num_4 > num_1 && num_4 > num_2 && num_2 > num_3 && num_4 > num_5){
+    //console.log(num_4 + " is greatest")
+//}
+//else if (num_5 > num_1 && num_5 > num_2 && num_5 > num_3 && num_5 > num_4){
+   // console.log(num_5 + " is greatest")
+//}
+//else{
+   // console.log("no number is greatest you have entered 2 equal numbers");
+//}
+
+
+//========Q4===========
+//for (i=0; i<=15; i++){
+    //if (i % 2 == 0 ){
+       // console.log(i + " : is even");
+    //}
+    //else{
+        //console.log(i + ": is odd");
+    //}
+//}
+
+//=========Q5=======
+//const english = 80;
+//const biology = 86;
+//const chemistry = 89;
+//const math = 90;
+//const history = 70;
+//const average = (english + biology + chemistry + math + history) / 5;
+//console.log("Average " + average);
+//if(average > 90 && average <= 100){
+   // console.log("Grade = A")
+//}
+//else if(average > 80 && average <= 90){
+    //console.log("Grade = B");
+//}
+//else if(average > 70 && average <= 80){
+   // console.log("Grade = C");
+//}
+//else if(average > 60 && average <= 70){
+   // console.log("D");
+//}
+//else if(average > 50 && average <= 60){
+    //console.log("F");
+///}
+//else{
+    //console.log("Fail");
+//}
+
+//==========Q6==========
+//for (i=1; i<=100; i++){
+    //if(i % 3 == 0 && i % 5 == 0){
+       // console.log(i + " FizzBuzz");
+    //}
+    //else if (i % 3 == 0){
+       // console.log(i + " Fuzz");
+    //}
+    //else if (i % 5 ==0){
+       // console.log(i + " Buzz");
+    //}
+//}
+
+
+//========Q7============
+//for(i = 1; i<=5; i++){
+    //for(j = 1; j<=i; j++ ){
+        //document.write("*")
+    //}
+    //document.write("<br>");
+//}
